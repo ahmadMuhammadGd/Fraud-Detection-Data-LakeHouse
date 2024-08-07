ad5438f2371429231acb1f431a7d2ff6e3c6d03f
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-08-07 04:50:01 +0300

    SSH spark integration

--- dockerfiles/spark/entrypoint.sh
+++ dockerfiles/spark/entrypoint.sh
@@ -18,7 +18,8 @@
 # under the License.
 
 start-master.sh -p 7077
-start-worker.sh spark://spark-iceberg:7077
+start-worker.sh spark://spark:7077
 start-history-server.sh
 start-thriftserver.sh  --driver-java-options "-Dderby.system.home=/tmp/derby"
+sudo service ssh start
 tail -f /dev/null
